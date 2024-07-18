adc1ad6de0aea389c690b3c4a64e072d4c47cc93
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-07-18 09:57:35 +0300

    add connect with mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
 import { config } from "dotenv";
 
 export function main() {
   config();
   const app = express();
   app.use(express.json());
+
+  mongoose
+    .connect(process.env["DB_URL"] as string)
+    .then(() => console.log("connected to db"))
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
 
   app.get("/hello", (req, res) => {
     res.status(200).send("is done");
