c71bdba5e9cc0354e858836e3f3cb56c799632f3
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-07-18 10:45:43 +0300

    added e2e test for create magic mover

---
+++ src/e2e/create-magic-item.spec.ts
@@ -0,0 +1,25 @@
+import { mockedCreateMagicItemPayload } from "../mock";
+import supertest from "supertest";
+
+export const baseUrl = "http://localhost:5000";
+
+describe("Creates a new  magic-item", () => {
+  it("Responses with 200 when the magic item is created successfully", async () => {
+    const { body, status } = await supertest(baseUrl)
+      .post("/magic-item")
+      .send(mockedCreateMagicItemPayload);
+    expect(status).toBe(201);
+    expect(body.name).toEqual(mockedCreateMagicItemPayload.name);
+  });
+
+  it.only("Response should throw bad request with status 400 ", async () => {
+    const { statusCode, body } = await supertest(baseUrl)
+      .post("/magic-item")
+      .send({
+        ...mockedCreateMagicItemPayload,
+        weight: "wrong",
+      });
+
+    expect(statusCode).toBe(400);
+  });
+});
