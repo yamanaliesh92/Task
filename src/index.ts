e40687e7c98017f0b0bfc0a57db42961e721e65a
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-07-18 09:55:55 +0300

    init index file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
-function main() {
-  console.log("hello");
+import express from "express";
+import { config } from "dotenv";
+
+export function main() {
+  config();
+  const app = express();
+  app.use(express.json());
+
+  app.get("/hello", (req, res) => {
+    res.status(200).send("is done");
+  });
+
+  app.listen(5000, () => {
+    console.log("listen to this this");
+  });
 }
+
 main();
